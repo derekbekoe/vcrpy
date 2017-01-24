f3174e60970c79fa05dedd28f9b9cf4726b7115c
Derek Bekoe <debekoe@microsoft.com>
2017-01-24 15:32:34 -0800

    Fix test failures (Python 3.6)
    AttributeError: type object 'VCRHTTPConnection/tmp/pytest-of-root/pytest-0/test' has no attribute '_get_content_length'

--- vcr/stubs/__init__.py
+++ vcr/stubs/__init__.py
@@ -199,6 +199,9 @@ class VCRConnection(object):
         # no need to check that here.
         self.real_connection.close()
 
+    def _get_content_length(self, request):
+        self.real_connection._get_content_length(request)
+
     def endheaders(self, message_body=None):
         """
         Normally, this would actually send the request to the server.
